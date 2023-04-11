ee84d28016127907d976f37e9903580deed9248d
haidoan4632 <doanhai01287181543@gmail.com>
2023-04-11 13:38:14 +0700

    nộp bài tập

---
+++ ss1_relation_database/ss1_student_management.sql
@@ -0,0 +1,12 @@
+create database student_management;
+use student_management;
+create table class (
+id int,
+name varchar(50)
+);
+create table teacher (
+id int,
+name varchar(50),
+age varchar(50),
+country varchar(50)
+);
