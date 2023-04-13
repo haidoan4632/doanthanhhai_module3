7bff1b3bfafe3c50508561c0f756f45a2379514d
haidoan4632 <doanhai01287181543@gmail.com>
2023-04-13 20:35:53 +0700

    nộp bài tập

--- ss3_thao_tac_voi_csdl/managerment_csdl.sql
+++ ss3_thao_tac_voi_csdl/managerment_csdl.sql
@@ -1,67 +1,67 @@
-drop database if exists QuanLySinhVien;
-CREATE DATABASE QuanLySinhVien;
-USE QuanLySinhVien;
-CREATE TABLE Class
+-- drop database if exists QuanLySinhVien;
+CREATE DATABASE quanlysinhvien;
+USE quanlysinhvien;
+CREATE TABLE classes
 (
-    ClassID   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    ClassName VARCHAR(60) NOT NULL,
-    StartDate DATETIME    NOT NULL,
-    Status    BIT
+    classid   INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    classname VARCHAR(60) NOT NULL,
+    startdate DATETIME NOT NULL,
+    status    BIT
 );
-CREATE TABLE Student
+CREATE TABLE student
 (
-    StudentId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    StudentName VARCHAR(30) NOT NULL,
-    Address     VARCHAR(50),
-    Phone       VARCHAR(20),
-    Status      BIT,
-    ClassId     INT         NOT NULL,
-    FOREIGN KEY (ClassId) REFERENCES Class (ClassID)
+    studentid   INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    studentname VARCHAR(30) NOT NULL,
+    address     VARCHAR(50),
+    phone       VARCHAR(20),
+    status      BIT,
+    classid     INT NOT NULL,
+    FOREIGN KEY (classid) REFERENCES classes (classid)
 );
-CREATE TABLE Subject
+CREATE TABLE subject
 (
-    SubId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    SubName VARCHAR(30) NOT NULL,
-    Credit  TINYINT     NOT NULL DEFAULT 1 CHECK ( Credit >= 1 ),
-    Status  BIT                  DEFAULT 1
+    subid   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    subName VARCHAR(30) NOT NULL,
+    credit  TINYINT     NOT NULL DEFAULT 1 CHECK ( Credit >= 1 ),
+    Status  BIT         DEFAULT 1
 );
-CREATE TABLE Mark
+CREATE TABLE mark
 (
-    MarkId    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    SubId     INT NOT NULL,
-    StudentId INT NOT NULL,
-    Mark      FLOAT   DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100),
-    ExamTimes TINYINT DEFAULT 1,
-    UNIQUE (SubId, StudentId),
-    FOREIGN KEY (SubId) REFERENCES Subject (SubId),
-    FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
+    markid    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    subid     INT NOT NULL,
+    studentid INT NOT NULL,
+    nark      FLOAT   DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100),
+    examtimes TINYINT DEFAULT 1,
+    UNIQUE (subid, studentid),
+    FOREIGN KEY (subid) REFERENCES subject (subid),
+    FOREIGN KEY (studentid) REFERENCES student (studentId)
 );
-INSERT INTO Class
+INSERT INTO classes
 VALUES (1, 'A1', '2008-12-20', 1);
-INSERT INTO Class
+INSERT INTO classes
 VALUES (2, 'A2', '2008-12-22', 1);
-INSERT INTO Class
+INSERT INTO classes
 VALUES (3, 'B3', current_date, 0);
 
-INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+INSERT INTO student (studentName, address, phone, status, clasid)
 VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
-INSERT INTO Student (StudentName, Address, Status, ClassId)
+INSERT INTO student (studentname, address, status, classid)
 VALUES ('Hoa', 'Hai phong', 1, 1);
-INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+INSERT INTO Student (studentname, address, phone, status, classid)
 VALUES ('Manh', 'HCM', '0123123123', 0, 2);
 
-INSERT INTO Subject
+INSERT INTO subject
 VALUES (1, 'CF', 5, 1),
        (2, 'C', 6, 1),
        (3, 'HDJ', 5, 1),
        (4, 'RDBMS', 10, 1);
        
-INSERT INTO Mark (SubId, StudentId, Mark, ExamTimes)
+INSERT INTO mark (subid, studentid, mark, examtimes)
 VALUES (1, 1, 8, 1),
        (1, 2, 10, 2),
        (2, 1, 12, 1);
 -- hiển thị tất cả sinh viên có tên bắt đầu bằng ký tự 'h'
-USE QuanLySinhVien;     
+USE quanlysinhvien;     
 SELECT * FROM student WHERE studentname LIKE "h%" ;
 -- hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
 SELECT * FROM class WHERE month(startdate)=12 ;
