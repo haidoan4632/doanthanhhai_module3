a13d31316f093fed0879fc7f474ad7867869e898
haidoan4632 <doanhai01287181543@gmail.com>
2023-04-14 20:27:18 +0700

    nộp bài tập

--- management_furama.sql
+++ management_furama.sql
@@ -3,28 +3,28 @@ CREATE DATABASE furama_management;
 USE furama_management;
 CREATE TABLE vi_tri (
     ma_vi_tri INT PRIMARY KEY AUTO_INCREMENT,
-    ten_vi_tri VARCHAR(45)
+    ten_vi_tri VARCHAR(45) NOT NULL
 );
  
 CREATE TABLE trinh_do (
     ma_trinh_do INT PRIMARY KEY AUTO_INCREMENT,
-    ten_trinh_do VARCHAR(45)
+    ten_trinh_do VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE bo_phan (
     ma_bo_phan INT PRIMARY KEY AUTO_INCREMENT,
-    ten_bo_phan VARCHAR(45)
+    ten_bo_phan VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE nhan_vien (
     ma_nhan_vien INT PRIMARY KEY AUTO_INCREMENT,
-    ho_ten VARCHAR(45),
-    ngay_sinh DATE,
-    so_cmnd VARCHAR(45),
+    ho_ten VARCHAR(45) NOT NULL, 
+    ngay_sinh DATE NOT NULL,
+    so_cmnd VARCHAR(45) UNIQUE,
     luong DOUBLE,
     so_dien_thoaii VARCHAR(45),
-    email VARCHAR(45),
-    dia_chi VARCHAR(45),
+    email VARCHAR(45) UNIQUE,
+    dia_chi VARCHAR(45) NOT NULL,
     ma_vi_tri INT,
     ma_trinh_do INT,
     ma_bo_phan INT,
@@ -37,19 +37,19 @@ CREATE TABLE nhan_vien (
 );
 CREATE TABLE loai_khach (
     ma_loai_khach INT PRIMARY KEY AUTO_INCREMENT,
-    ten_loai_khach VARCHAR(45)
+    ten_loai_khach VARCHAR(45) UNIQUE
 );
 
 CREATE TABLE khach_hang (
     ma_khach_hang INT PRIMARY KEY AUTO_INCREMENT,
     ma_loai_khach INT,
-    ho_ten VARCHAR(45),
-    ngay_sinh DATE,
-    gioi_tinh BIT(1),
-    so_cmnd VARCHAR(45),
-    so_dien_thoai VARCHAR(45),
-    email VARCHAR(45),
-    dia_chi VARCHAR(45),
+    ho_ten VARCHAR(45) NOT NULL,
+    ngay_sinh DATE NOT NULL,
+    gioi_tinh BIT(1) NOT NULL,
+    so_cmnd VARCHAR(45) UNIQUE,
+    so_dien_thoai VARCHAR(45) UNIQUE,
+    email VARCHAR(45) UNIQUE,
+    dia_chi VARCHAR(45) UNIQUE,
     FOREIGN KEY (ma_loai_khach)
         REFERENCES loai_khach (ma_loai_khach)
 );
@@ -60,21 +60,21 @@ CREATE TABLE khach_hang (
 
 CREATE TABLE loai_dich_vu (
     ma_loai_dich_vu INT PRIMARY KEY AUTO_INCREMENT,
-    ten_loai_dich_vu VARCHAR(45)
+    ten_loai_dich_vu VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE dich_vu (
     ma_dich_vu INT PRIMARY KEY AUTO_INCREMENT,
-    ten_dich_vu VARCHAR(45),
-    dien_tich INT,
+    ten_dich_vu VARCHAR(45) UNIQUE,
+    dien_tich INT NOT NULL,
     chi_phi_thue DOUBLE,
-    so_nguoi_toi_da INT,
+    so_nguoi_toi_da INT NOT NULL,
     ma_kieu_thue INT,
     ma_loai_dich_vu INT,
-    tieu_chuan_phong VARCHAR(45),
-    mo_ta_tien_nghi_khac VARCHAR(45),
-    dien_tich_ho_boi DOUBLE,
-    so_tang INT,
+    tieu_chuan_phong VARCHAR(45) UNIQUE,
+    mo_ta_tien_nghi_khac VARCHAR(45) UNIQUE,
+    dien_tich_ho_boi DOUBLE UNIQUE,
+    so_tang INT NOT NULL,
     dich_vu_mien_phi_di_kem TEXT,
     FOREIGN KEY (ma_kieu_thue)
         REFERENCES kieu_thue (ma_kieu_thue),
@@ -88,8 +88,8 @@ CREATE TABLE dich_vu (
 
 CREATE TABLE hop_dong (
     ma_hop_dong INT PRIMARY KEY AUTO_INCREMENT,
-    ngay_lam_hop_dong DATETIME,
-    ngay_ket_thuc DATETIME,
+    ngay_lam_hop_dong DATETIME NOT NULL,
+    ngay_ket_thuc DATETIME NOT NULL,
     tien_dat_coc DOUBLE,
     ma_nhan_vien INT,
     ma_khach_hang INT,
@@ -119,12 +119,12 @@ CREATE TABLE hop_dong_chi_tiet (
 
 CREATE TABLE kieu_thue (
     ma_kieu_thue INT PRIMARY KEY AUTO_INCREMENT,
-    ten_kieu_thue VARCHAR(45)
+    ten_kieu_thue VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE dich_vu_di_kem (
     ma_dich_vu_di_kem INT PRIMARY KEY AUTO_INCREMENT,
-    ten_dich_vu_di_kem VARCHAR(45),
+    ten_dich_vu_di_kem VARCHAR(45) NOT NULL,
     gia DOUBLE,
     don_vi VARCHAR(10),
     trang_thai VARCHAR(45)
@@ -180,5 +180,55 @@ ORDER BY so_lan_dat_phong;
 -- tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet)
 -- cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
 
+SELECT k.ma_khach_hang, k.ho_ten, l.ten_loai_khach, h.ma_hop_dong, d.ten_dich_vu, h.ngay_lam_hop_dong, h.ngay_ket_thuc, (ifnull(d.chi_phi_thue,0) + sum(ifnull(hd.so_luong,0) * ifnull(dv.gia,0))) as tong_tien
+FROM khach_hang k 
+LEFT JOIN hop_dong h ON k.ma_khach_hang = h.ma_khach_hang 
+LEFT JOIN loai_khach l ON l.ma_loai_khach = k.ma_loai_khach
+LEFT JOIN hop_dong_chi_tiet hd ON hd.ma_hop_dong = h.ma_hop_dong
+LEFT JOIN dich_vu d ON d.ma_dich_vu = h.ma_dich_vu 
+LEFT JOIN dich_vu_di_kem dv ON dv.ma_dich_vu_di_kem = hd.ma_dich_vu_di_kem 
+GROUP BY h.ma_hop_dong, k.ma_khach_hang;
+
+-- task 6 đến task 10
+
+-- task 6: Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của 
+-- tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+SELECT dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich, dich_vu.chi_phi_thue, loai_dich_vu.ten_loai_dich_vu
+FROM dich_vu 
+INNER JOIN loai_dich_vu ON dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+INNER JOIN hop_dong  ON dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+WHERE dich_vu.ma_dich_vu NOT IN (SELECT ma_dich_vu FROM hop_dong
+WHERE year(hop_dong.ngay_lam_hop_dong) = 2021
+AND (month(hop_dong.ngay_lam_hop_dong) BETWEEN 1 AND 3))
+GROUP BY ma_dich_vu ;
+
+-- task 7:	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue,
+-- ten_loai_dich_vu của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng
+-- chưa từng được khách hàng đặt phòng trong năm 2021.
 
 
+
+-- task 8: Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+-- cách 1:
+SELECT ho_ten 
+FROM khach_hang
+GROUP BY ho_ten;
+-- cách 2:
+SELECT DISTINCT ho_ten 
+FROM khach_hang;
+
+-- cách 3: 
+SELECT ho_ten 
+FROM khach_hang
+UNION
+SELECT ho_ten 
+FROM khach_hang
+
+-- task 9: 	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 
+-- thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+
+SELECT COUNT(ma_khach_hang) AS so_luong_khach_dat_phong, MONTH(ngay_lam_hop_dong) AS thang
+FROM hop_dong
+WHERE YEAR(ngay_lam_hop_dong) = 2021
+GROUP BY thang
+ORDER BY thang;
