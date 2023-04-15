6aa4a9d02866e3b349c2b3f69dc15f5979c962cd
haidoan4632 <doanhai01287181543@gmail.com>
2023-04-15 09:45:59 +0700

    nộp bài tập

--- management_furama.sql
+++ management_furama.sql
@@ -206,6 +206,13 @@ GROUP BY ma_dich_vu ;
 -- ten_loai_dich_vu của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng
 -- chưa từng được khách hàng đặt phòng trong năm 2021.
 
+SELECT dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.so_nguoi_toi_da, dv.chi_phi_thue, ldv.ten_loai_dich_vu
+from dich_vu dv
+LEFT JOIN hop_dong hd ON dv.ma_dich_vu = hd.ma_dich_vu
+LEFT JOIN loai_dich_vu ldv ON ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+WHERE dv.ma_dich_vu IN (SELECT ma_dich_vu FROM hop_dong WHERE year(hd.ngay_lam_hop_dong) = 2020) 
+AND (dv.ma_dich_vu NOT IN (SELECT ma_dich_vu FROM hop_dong where year(hd.ngay_lam_hop_dong) = 2021))
+GROUP BY dv.ma_dich_vu;
 
 
 -- task 8: Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
@@ -218,11 +225,9 @@ SELECT DISTINCT ho_ten
 FROM khach_hang;
 
 -- cách 3: 
-SELECT ho_ten 
-FROM khach_hang
+SELECT ho_ten FROM khach_hang
 UNION
-SELECT ho_ten 
-FROM khach_hang
+SELECT ho_ten FROM khach_hang
 
 -- task 9: 	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 
 -- thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
@@ -232,3 +237,13 @@ FROM hop_dong
 WHERE YEAR(ngay_lam_hop_dong) = 2021
 GROUP BY thang
 ORDER BY thang;
+-- task 10: Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
+-- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, 
+-- so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+
+SELECT hd.ma_hop_dong, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, hd.tien_dat_coc, sum(ifnull(hdct.so_luong,0)) as so_luong_dich_vu_di_kem
+FROM hop_dong hd
+LEFT JOIN dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu
+LEFT JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+GROUP BY hd.ma_hop_dong;
+
