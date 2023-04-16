b7b50d3a03f07fc7a9e3a0141125643bf3f7f0f2
haidoan4632 <doanhai01287181543@gmail.com>
2023-04-16 21:07:38 +0700

    nộp bài tập

--- management_furama.sql
+++ management_furama.sql
@@ -247,3 +247,55 @@ LEFT JOIN dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu
 LEFT JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
 GROUP BY hd.ma_hop_dong;
 
+-- task 11:	Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” 
+-- và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+
+SELECT dv.ma_dich_vu_di_kem, dv.ten_dich_vu_di_kem, dv.gia, dv.don_vi, dv.trang_thai FROM dich_vu_di_kem dv
+JOIN hop_dong_chi_tiet hdct ON dv.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+JOIN hop_dong hd ON hd.ma_hop_dong = hdct.ma_hop_dong
+JOIN khach_hang kh ON kh.ma_khach_hang =  hd.ma_khach_hang
+JOIN loai_khach lk ON lk.ma_loai_khach = kh.ma_loai_khach
+WHERE lk.ten_loai_khach = "Diamond" AND (kh.dia_chi LIKE '%Vinh%' OR  kh.dia_chi LIKE '%Quảng Ngãi%');
+
+-- task 12:	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), 
+-- ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem),
+-- tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 
+-- nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+
+SELECT hd.ma_hop_dong, nv.ho_ten as ho_ten_nhan_vien , kh.ho_ten as ho_ten_khach_hang, kh.so_dien_thoai as so_dien_thoai_khach_hang,dv.ten_dich_vu, SUM(ifnull(hdct.so_luong,0)) as so_luong_dich_vu_di_kem,hd.tien_dat_coc
+FROM hop_dong_chi_tiet hdct
+LEFT JOIN hop_dong hd ON hdct.ma_hop_dong = hd.ma_hop_dong
+LEFT JOIN nhan_vien nv ON nv.ma_nhan_vien = hd.ma_nhan_vien
+LEFT JOIN khach_hang kh ON kh.ma_khach_hang = hd.ma_khach_hang 
+LEFT JOIN dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
+WHERE year(hd.ngay_lam_hop_dong) = 2020 AND(MONTH(hd.ngay_lam_hop_dong)BETWEEN 10 AND 12) 
+AND hd.ngay_lam_hop_dong NOT IN (SELECT ngay_lam_hop_dong FROM hop_dong WHERE year(hd.ngay_lam_hop_dong) = 2021 AND (MONTH(hd.ngay_lam_hop_dong) BETWEEN 1 AND 6))
+GROUP BY hd.ma_hop_dong;
+
+-- task 12 bị mất đi 1 khách hàng tên dương văn quan
+
+
+-- task 13:	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
+-- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+
+
+-- task 14: Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
+SELECT hd.ma_hop_dong, ldv.ten_loai_dich_vu, dvdk.ten_dich_vu_di_kem, count(hdct.ma_dich_vu_di_kem) as so_lan_su_dung
+FROM hop_dong hd
+JOIN dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
+RIGHT JOIN hop_dong_chi_tiet hdct ON hdct.ma_hop_dong = hd.ma_hop_dong
+JOIN loai_dich_vu ldv ON ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+JOIN dich_vu_di_kem dvdk ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+GROUP BY hdct.ma_dich_vu_di_kem
+HAVING so_lan_su_dung = 1;
+
+-- task 15: Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, 
+-- so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+SELECT nv.ma_nhan_vien, nv.ho_ten, td.ten_trinh_do, bp.ten_bo_phan, nv.so_dien_thoaii, nv.dia_chi
+FROM hop_dong hd
+JOIN nhan_vien nv ON nv.ma_nhan_vien = hd.ma_nhan_vien
+JOIN trinh_do td ON td.ma_trinh_do = nv.ma_trinh_do
+JOIN bo_phan bp ON bp.ma_bo_phan = nv.ma_bo_phan
+GROUP BY hd.ma_nhan_vien
+HAVING COUNT(hd.ma_nhan_vien) <= 3;
